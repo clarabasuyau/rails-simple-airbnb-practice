dc5874e5f9c199cc93cb2db91836d099f65f672b
clara basuyau <clarabasuyau@gmail.com>
2022-09-19 16:42:08 +0200

    add form to create a new flat

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -18,7 +18,7 @@ class FlatsController < ApplicationController
 
   private
   def flat_params
-    params.require(:flat).permit(:name, :address, :description, :price_per_night, :price, :overview, :number_of_guests)
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
   end
 end
 
