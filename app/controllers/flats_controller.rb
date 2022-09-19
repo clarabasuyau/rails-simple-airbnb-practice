535a1d4d94580464d957e795a234ad0608f6324b
clara basuyau <clarabasuyau@gmail.com>
2022-09-19 16:20:55 +0200

    recreate simple airbnb

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,39 @@
+class FlatsController < ApplicationController
+  def index
+    @flats = Flat.all
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flats_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :price, :overview, :number_of_guests)
+  end
+end
+
+# def edit
+#   @car = Car.find(params[:id])
+# end
+
+# def update
+#   @car = Car.find(params[:id])
+#   @car.update(car_params)
+#   redirect_to car_path(@car)
+# end
+
+# def destroy
+#   @car = Car.find(params[:id])
+#   @car.destroy
+#   redirect_to cars_path, status: :see_other
+# end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Creating first flat"
+
+Flat.destroy_all
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+5.times do
+  Flat.create!(
+    name: Faker::Fantasy::Tolkien.poem,
+    address: Faker::Fantasy::Tolkien.location,
+    description: Faker::GreekPhilosophers.quote,
+    price_per_night: (1..80).to_a.sample,
+    number_of_guests: (1..5).to_a.sample
+  )
+end
+
+puts "creating the faker seeds!"
